b35a7fc4f07e8143bb5870daa15bf0237fb725b0
Anjana AS <144725021+Anjana-AS@users.noreply.github.com>
2025-12-08 14:22:47 +0530

    Refactor vowel counting program
    Removed unused special character array and related code.

--- vowel.c
+++ vowel.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
-#include <string.h>
 #include <ctype.h>
 
 int main() {
 char str[1000];
-char specialchars[1000];  
+  
 int a = 0, e = 0, vowel_i = 0, o = 0, u = 0;
-int specialcount = 0, spclchar = 0;
+int specialcount = 0;
 printf("Enter a string: ");
 fgets(str, sizeof(str), stdin);  
+
+printf("\nspecial characters found:");
 for (int i = 0; str[i] != '\0'; i++) {
 char ch = tolower(str[i]);
+
 switch (ch) {
 case 'a': a++; break;
 case 'e': e++; break;
@@ -19,13 +21,13 @@ case 'o': o++; break;
 case 'u': u++; break;
 default:
 if (!isalnum(ch) && !isspace(ch)) {
-specialchars[spclchar++] = str[i];
+printf("%c",str[i]);
 specialcount++;
 }
 break;
 }
 }
-specialchars[spclchar] = '\0';
+
 printf("\nVowel Occurrences:\n");
 printf("a: %d\n", a);
 printf("e: %d\n", e);
@@ -33,9 +35,6 @@ printf("i: %d\n", vowel_i);
 printf("o: %d\n", o);
 printf("u: %d\n", u);
 printf("\nSpecial Characters Count: %d\n", specialcount);
-if (specialcount > 0) {
-printf("Special Characters Found: %s\n", specialchars);
-}
+
 return 0;
 }
-
